3725c7aaf69f9df85ea5f90874d878e38a0199b4
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-04-02 10:50:03 +0300

    added changes

--- app.py
+++ app.py
@@ -2,22 +2,17 @@
 from flask import Flask
 from flask_migrate import Migrate
 from models import db
-import routes  # Import routes to register them
+import routes
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Configure the database URI
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///heroes.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Disable modification tracking
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Initialize the database and migration
 db.init_app(app)
 migrate = Migrate(app, db)
 
-# Import routes to ensure they are registered when the app starts
 import routes
 
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
